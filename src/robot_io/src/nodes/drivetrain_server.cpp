837b575ddc57acb4239bbb6be830ca1781aa990a
Damien Prieur <dprieur834@gmail.com>
2020-04-02 04:41:10 +0000

    Drivetrain:Implemented Steering and added get status msg Get status has power implemented, needs steering angle

--- src/robot_io/src/nodes/drivetrain_server.cpp
+++ src/robot_io/src/nodes/drivetrain_server.cpp
@@ -1,5 +1,6 @@
 #include <ros/ros.h>
-#include <robot_io/DriveTrainRequest.h>
+#include <robot_io/DrivetrainRequest.h>
+#include <robot_io/DrivetrainStatus.h>
 #include <fcntl.h>
 #include <sys/ioctl.h>
 #include <stdio.h>
@@ -9,10 +10,14 @@ extern "C"{
     #include "drivers/TB6612FNG/TB6612FNG.h"
 }
 
-static const std::string SERVICE ="drive_train_request";
+static const std::string SERVICE ="drivetrain_request";
+static const std::string STATUS_PUBSHISHER ="drivetrain_status";
 
-typedef robot_io::DriveTrainRequest::Request  DriveTrainRequest;
-typedef robot_io::DriveTrainRequest::Response DriveTrainResponse;
+/* 90 degrees is the middle, and the rest is offset for the particular servo */
+static const uint16_t DRIVETRAIN_STRAIGHT_ANGLE = 90 - 10;
+
+typedef robot_io::DrivetrainRequest::Request  DrivetrainRequest;
+typedef robot_io::DrivetrainRequest::Response DrivetrainResponse;
 
 typedef SF0180 Servo;
 typedef TB6612FNG MotorController;
@@ -21,18 +26,44 @@ typedef TB6612FNG MotorController;
 static Servo steer_servo;
 static MotorController drive_motors;
 
-bool drivetrain_request_handler(DriveTrainRequest &req, DriveTrainResponse &res)
+bool drivetrain_request_handler(DrivetrainRequest &req, DrivetrainResponse &res)
 {
     int status;
 
-    status = TB6612FNG_set_speed(drive_motors, 0, req.power/100.);
+    status  = TB6612FNG_set_speed(drive_motors, 0, req.power/100.);
     status |= TB6612FNG_set_speed(drive_motors, 1, req.power/100.);
     if(status){
         ROS_WARN("Drivetrain: failed to set speed");
         return false;
     }
 
+    /* angle range is [-45, 45] degrees */
+    if(req.angle < -45){
+        req.angle = -45;
+        ROS_WARN("Drivetrain: Steering angle range is [-45, 45] degrees");
+    }else if(req.angle > 45){
+        req.angle = 45;
+        ROS_WARN("Drivetrain: Steering angle range is [-45, 45] degrees");
+    }
+
+    /* positive angle is right, negative is left */
+    if(SF0180_set_angle(steer_servo, 180 - DRIVETRAIN_STRAIGHT_ANGLE + req.angle)){
+        ROS_WARN("Drivetrain: failed to set steering angle");
+        return false;
+    }
+
     return true;
+}
+
+void drivetrain_status_publish(ros::Publisher &pub){
+    robot_io::DrivetrainStatus status;
+    float speed;
+    if(TB6612FNG_get_speed(drive_motors, 0, &speed)){
+        return;
+    }
+    status.power = speed*100;
+    status.angle = 0;
+    pub.publish(status);
 }
 
 
@@ -51,12 +82,18 @@ int init_drivetrain()
         return -1;
     }
 
-    int status = PCA9685_initialize(drive_motors.pwm_dev);
-    if(status){
-        ROS_ERROR("Drivetrain: Failed to initialize PWM driver:%d", status);
+    /* the pwm controllers should be pointers so that multiple devices can use one device */
+    steer_servo.pwm_dev.i2c_dev_fd = drive_motors.pwm_dev.i2c_dev_fd;
+    /* which pin is it using */
+    steer_servo.pwm = 2;
+
+    /* this initializes the PWM controller, maybe not a good idea */
+    if(SF0180_initialize(steer_servo)){
+        ROS_ERROR("Drivetrain: Failed to initialize Steering servo");
         return 1;
     }
 
+    /* which pins are they using */
     drive_motors.motor[0].in1 = 17;
     drive_motors.motor[0].pwm = 5;
     drive_motors.motor[1].in1 = 27;
@@ -68,6 +105,7 @@ int init_drivetrain()
         return 1;
     }
 
+
     return 0;
 }
 
@@ -76,6 +114,7 @@ int main(int argc, char** argv)
 {
     ros::init(argc, argv, "drivetrain_node");
     ros::NodeHandle n;
+    ros::Rate loop_rate(20);
 
 
     if(init_drivetrain()){
@@ -85,9 +124,18 @@ int main(int argc, char** argv)
 
 
     ros::ServiceServer service = n.advertiseService(SERVICE, drivetrain_request_handler);
+    ros::Publisher drivetrain_status_pub = n.advertise<robot_io::DrivetrainStatus>(STATUS_PUBSHISHER, 1);
+
     ROS_INFO("Drivetrain: Ready");
 
-    ros::spin();
+    while(ros::ok()){
+        drivetrain_status_publish(drivetrain_status_pub);
+
+        /* Should spin for duration instead of sleep */
+        ros::spinOnce();
+
+        loop_rate.sleep();
+    }
 
     TB6612FNG_close(drive_motors);
     close(drive_motors.pwm_dev.i2c_dev_fd);
